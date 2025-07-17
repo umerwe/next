a8b29f02de56620307870deac4d5c7935ce1536e
umerwe <uemyy1@gmail.com>
2025-07-17 16:22:57 +0500

    refactor: adjust layout of rating section for improved alignment

--- src/components/pages/details/details.tsx
+++ src/components/pages/details/details.tsx
@@ -61,7 +61,7 @@ const Details = ({ id }: DetailsProps) => {
 
 
                     {/* Rating */}
-                    <div className="flex flex-col sm:flex-row sm:items-center gap-2 sm:gap-2">
+                    <div className="flex flex-row sm:items-center gap-2 sm:gap-2">
                         <div className="flex items-center space-x-2">
                             <span className="bg-green-700 text-white px-2 py-1 rounded text-xs">{property.rating.toFixed(1)}</span>
                             <span className="font-semibold text-gray-700">Excellent</span>
